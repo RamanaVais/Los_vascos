c0e4a8772ba2ff5358cadae5d454a6c49496fe8d
Ramana <fengxiyuan@foxmail.com>
2018-09-17 01:33:21 +0800

    this is a important changes

--- 2.py
+++ 2.py
@@ -3,5 +3,35 @@ hello please go
 very difference_update
 
 
+this is so cool 
 change 1
 another change 1
+[33mcommit 12e6177d1fd2175c255bfd2a5ea455d7490c65e1[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Ramana <fengxiyuan@foxmail.com>
+Date:   Mon Sep 17 01:28:22 2018 +0800
+
+    change 4
+
+[33mcommit e40bd807815dba0943e6f8981bea752d443c3e4e[m
+Author: Ramana <fengxiyuan@foxmail.com>
+Date:   Mon Sep 17 01:22:56 2018 +0800
+
+    change 3
+
+[33mcommit e9816a490fb72398f35a41ce2d5157dbc8939c4a[m
+Author: Ramana <fengxiyuan@foxmail.com>
+Date:   Mon Sep 17 01:19:14 2018 +0800
+
+    change 2
+
+[33mcommit f280f9644a93f109b63162747a9b263cd1ae0bcb[m
+Author: Ramana <fengxiyuan@foxmail.com>
+Date:   Mon Sep 17 01:16:53 2018 +0800
+
+    change 1 hello world
+
+[33mcommit 47aca8598f2ccdd567bead24504e08271898edaf[m
+Author: Ramana <fengxiyuan@foxmail.com>
+Date:   Mon Sep 17 01:05:47 2018 +0800
+
+    this will be success
